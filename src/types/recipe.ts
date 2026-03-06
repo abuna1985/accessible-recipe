a870a0a681920c15d79e9d12477bcf94ccf6d111
Adam Abundis <adamjabundis@gmail.com>
2026-03-06 14:57:44 -0800

    feat: add recipe type definitions

--- src/types/recipe.ts
+++ src/types/recipe.ts
@@ -1,1 +1,42 @@
- 
+export type TimeUnit = "minutes" | "hours"
+
+export interface Time {
+  duration: number
+  unit: T
+}
+
+export const UNITS = [
+  "cup",
+  "tbsp",
+  "tsp",
+  "fl_oz",
+  "ml",
+  "l",
+  "oz",
+  "lb",
+  "g",
+  "kg",
+  "pinch",
+  "dash",
+] as const
+
+export type Unit = typeof UNITS[number]
+
+export interface Ingredient {
+  quantity: number
+  unit?: Unit
+  name: string
+  note?: string
+}
+
+export interface Recipe {
+  title: string
+  description?: string
+  servings: number
+  totalTime: Time
+  prepTime?: Time
+  cookTime?: Time
+  ingredients: Ingredient[]
+  steps: string[]
+  nutrition?: NutritionItem[]
+}
